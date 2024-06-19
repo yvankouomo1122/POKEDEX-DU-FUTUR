1bfb46195ce749764d24a7d3fe1ce8716265d797
yvankouomo1122 <113889187+yvankouomo1122@users.noreply.github.com>
2024-06-19 03:08:40 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+    content: [
+      './resources/views/**/*.blade.php',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+
